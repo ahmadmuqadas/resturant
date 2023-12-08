57aebfbd46ac573d602e332625385db4c0d48289
ahmadmuqadas <ahmadmuqadas.98@gmail.com>
2023-12-08 15:09:12 -0800

    added more functionality to the navbar - close the navbar on mobile after every new route visit. - close the navbar when clicked outside the navbar.

--- src/Assets/User components/MainNav.jsx
+++ src/Assets/User components/MainNav.jsx
@@ -1,90 +1,99 @@
-import React, { useEffect, useMemo, useState } from 'react'
-import { NavLink, Outlet, useLocation } from 'react-router-dom'
-import Logo from '../Media/logo.png'
+import React, { useEffect, useMemo, useState, useRef } from 'react';
+import { NavLink, Outlet, useLocation } from 'react-router-dom';
+import Logo from '../Media/logo.png';
 import Footer from './Footer';
 
-
-
-
-
-
-
-
 const MainNav = () => {
-
-  const {pathname} = useLocation();
-
-
-
+  const { pathname } = useLocation();
   const isSmallScreen = window.matchMedia('(max-width: 768px)').matches;
- 
 
   const [toggle, setToggle] = useState(false);
   const [scrollPos, setScrollPos] = useState(0);
+
+  const navRef = useRef(null);
+
+
+  useEffect(() => {
+    setToggle(false); // Close the navigation panel
+  }, [ pathname ]);
+  
 
   useEffect(() => {
     const handleScroll = () => {
       setScrollPos(window.scrollY);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    const handleClickOutside = (event) => {
+      if (navRef.current && !navRef.current.contains(event.target)) {
+        setToggle(false);
+      }
+    };
 
-   
+    window.addEventListener('scroll', handleScroll);
+    document.addEventListener('mousedown', handleClickOutside);
+
     return () => {
       window.removeEventListener('scroll', handleScroll);
+      document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, []); //
+  }, []);
 
+  function toggler() {
+    setToggle((prev) => !prev);
+  }
 
-function toggler() {
-setToggle((prev) => {
-return !prev
-})
-}
+  const toggleCondition = useMemo(() => {
+    return {
+      display: toggle ? 'block' : 'none',
+      background: scrollPos > 50 || isSmallScreen ? '#f77f00' : 'transparent',
+    };
+  }, [toggle, scrollPos, isSmallScreen]);
 
-const toggleCondition = useMemo(() => {
-  return {
-    display: toggle ? 'block' : 'none',
-    background: scrollPos > 50 || isSmallScreen === true ? ' #f77f00' : 'transparent',
-   
-    
-  
-
+  const navStyles = {
+    position: pathname !== '/' ? 'static' : 'sticky',
+    height: pathname !== '/' ? '5rem' : '0',
   };
-}, [toggle, scrollPos, isSmallScreen]);
 
-const navStyles = {
-  position: pathname !== '/' ? 'static' : 'sticky',
-  height: pathname !== '/' ? '5rem' : '0',
-}
+  const liStyles = {
+    color: pathname !== '/' ? 'black' : '',
+  };
 
-const liStyles = {
-  color: pathname !== '/' ? 'black' : '',
-}
   return (
     <>
-    <nav style={navStyles} >
-<div className="burger-container">
-<img src={Logo} alt="logo" className='mobile-logo' onClick={toggler} />
-      <p className='logo-name'>Delecious</p>
-</div>
-        <ul className='navigation-ul' style={toggleCondition}>
-        
-           <NavLink><li style={liStyles}>HOME</li></NavLink>
-           <NavLink><li style={liStyles}>ORDER NOW</li></NavLink>
-           <NavLink><li style={liStyles}>RESERVATION</li></NavLink>
-           <NavLink><li style={liStyles}>MENU</li></NavLink>
-           <NavLink><li style={liStyles}>FEEDBACK</li></NavLink>
-           <NavLink><li style={liStyles}>CONTACT</li></NavLink>
-           <NavLink to='login'><li style={liStyles}>LOG IN</li></NavLink>
+      <nav style={navStyles} ref={navRef}>
+        <div className="burger-container">
+          <img src={Logo} alt="logo" className="mobile-logo" onClick={toggler} />
+          <p className="logo-name">Delecious</p>
+        </div>
+        <ul className="navigation-ul" style={toggleCondition}>
+          <NavLink>
+            <li style={liStyles}>HOME</li>
+          </NavLink>
+          <NavLink>
+            <li style={liStyles}>ORDER NOW</li>
+          </NavLink>
+          <NavLink>
+            <li style={liStyles}>RESERVATION</li>
+          </NavLink>
+          <NavLink>
+            <li style={liStyles}>MENU</li>
+          </NavLink>
+          <NavLink>
+            <li style={liStyles}>FEEDBACK</li>
+          </NavLink>
+          <NavLink>
+            <li style={liStyles}>CONTACT</li>
+          </NavLink>
+          <NavLink to="login">
+            <li style={liStyles}>LOG IN</li>
+          </NavLink>
         </ul>
-    </nav>
+      </nav>
 
-    <Outlet/>
-    <Footer/>
-    
+      <Outlet />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default MainNav
+export default MainNav;
