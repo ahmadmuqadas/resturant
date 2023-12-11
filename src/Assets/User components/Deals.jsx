2929c65149257979b67cb1c498e6b64ff2abd2b1
ahmadmuqadas <ahmadmuqadas.98@gmail.com>
2023-12-11 13:53:35 -0800

    added the modal and its functionality

--- src/Assets/User components/Deals.jsx
+++ src/Assets/User components/Deals.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import { database } from '../../FirebaseConfig';
 import { get, ref } from 'firebase/database';
 import { useLoaderData } from 'react-router-dom';
-
+import closeImg from '../Media/close.png'
 
 
 
@@ -33,50 +33,54 @@ export function loader() {
 
 
 
-
-
-
 const Deals = () => {
- 
-
-
 const menu = useLoaderData();
+const [isModalOpen, SetIsModalOpen] = useState(false);
 
+const modalStyle = {
+  display: isModalOpen ? 'block' : 'none'
+}
+
+function modalOpener() {
+  SetIsModalOpen((prev) => !prev)
+}
 const menuArray = menu.foodCategories;
-
+function modal() {
+  console.log('function fired');
+  SetIsModalOpen((prev) => !prev)
+}
   const categories = menuArray.map((category) => {
 
     return (
       
       <motion.div
-      initial={{ y: 200, opacity: 0 }}
-      whileInView={{ y: 0, opacity: 1 }}
-      transition={{ delay: 0.3, duration: .5 }} 
-      viewport={{ once: true }}
-       className='foods-cat'>
+        key={category.name}  
+        initial={{ y: 200, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.3, duration: 0.5 }}
+        viewport={{ once: true }}
+        className='foods-cat'
+      >
         <h2 className='cat-name'>{category.category}</h2>
-          <div className='food-wrapper'>
-          {category.foods.map((food) => (
-            <div className='foods'>
-         <div className='food-img-wrapper'>
-          <img src={food.img} alt="" className='deal-img' />
-    <span className='serving-span'>
-    <img src={serving} alt="" className='serving-img' />
-          <p  className='food-item serving'>X {food.serves}</p>
-    </span>
-          </div> 
-
-         <div className="food-item-wrapper">
-         <p  className='food-item food-name'>{food.name}</p>
-          <p className='food-item food-price'>{food.price}</p>
-          <button className='select-btn'>Select</button>
-        
-         </div>
-          </div>
-        
-        ))}
-          </div>
-        
+        <div className='food-wrapper'>
+          {category.foods.map((food, foodIndex) => (
+            <div key={foodIndex} className='foods'>
+              <div className='food-img-wrapper'>
+                <img src={food.img} alt="" className='deal-img' />
+                <span className='serving-span'>
+                  <img src={serving} alt="" className='serving-img' />
+                  <p className='food-item serving'>X {food.serves}</p>
+                </span>
+              </div>
+              <div className="food-item-wrapper">
+                <p className='food-item food-name'>{food.name}</p>
+                <p className='food-item food-price'>{food.price}</p>
+                <button className='select-btn' onClick={modal}>Select</button>
+              
+              </div>
+            </div>
+          ))}
+        </div>
       </motion.div>
     );
   });
@@ -84,6 +88,17 @@ const menuArray = menu.foodCategories;
   return (
     <div className='deals-wrapper'>
       {categories}
+      <div className='modal' style={modalStyle}>
+        <div className="delievery-wrapper">
+          <div className="delivery">
+            delivery
+          </div>
+          <div className="carryOut">
+            carry out
+          </div>
+        </div>
+        <img src={closeImg} className='closeImg' alt="close" onClick={modalOpener}/>
+      </div>
       </div>
   )
 }
